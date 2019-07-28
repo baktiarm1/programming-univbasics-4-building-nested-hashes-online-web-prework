9c25c444e0ce57bb778dcd4f851d16b94c796dbc
Baktiar Miah <baktiar.m@gmail.com>
2019-07-28 16:55:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -41,14 +41,14 @@ end
 
 def monopoly_with_fourth_tier
 monopoly =  {
-  :railroads=>
+  :railroads =>
   {
-    :pieces=>4,
-    :rent_in_dollars=>
+    :pieces =>4,
+    :rent_in_dollars =>
     {
-      :one_piece_owned=>25,
-      :two_pieces_owned=>50,
-      :three_pieces_owned=>100,
+      :one_piece_owned =>25,
+      :two_pieces_owned =>50,
+      :three_pieces_owned =>100,
       :four_pieces_owned =>200
     },
     :names=>
